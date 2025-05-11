b534ec102e1d1d0dabc43b0e3b399edef75a810f
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-05-11 21:15:58 +0300

    Modify the DashboardController

--- src/main/java/com/example/blablacar/controller/DashboardController.java
+++ src/main/java/com/example/blablacar/controller/DashboardController.java
@@ -1,10 +1,18 @@
 package com.example.blablacar.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 /**
- * Controller for the dashboard page
+ * Controller for authentication-related endpoints
  */
-//@RestController
+@RestController
 public class DashboardController {
 
 
+    @GetMapping("/greeting")
+    public Map<String, String> home() {
+        return Map.of("message", "Welcome, authenticated user!");
+    }
 }
