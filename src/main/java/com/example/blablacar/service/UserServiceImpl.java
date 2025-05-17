28563092c3f3863fdcb46c4dd611fd7091302110
alexandruDicu <alexdicu09@yahoo.com>
2025-05-17 16:08:07 +0300

    Some config changes

--- src/main/java/com/example/blablacar/service/UserServiceImpl.java
+++ src/main/java/com/example/blablacar/service/UserServiceImpl.java
@@ -17,8 +17,6 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public User registerUser(User user) {
-        System.out.println("🔐 Encoding and saving: " + user.getEmail());
-        System.out.println("🔐 Password: " + user.getPassword());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
