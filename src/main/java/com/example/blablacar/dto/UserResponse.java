32c8a8189f67308984aeccf4a51db17f6c883686
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-05-25 17:35:37 +0300

    Add UserResponse

---
+++ src/main/java/com/example/blablacar/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package com.example.blablacar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String email;
+}
