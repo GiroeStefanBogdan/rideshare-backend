e22bcd0f05e07d3c7607df63cf5d8ba4d082ab2b
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-06-13 20:33:49 +0300

    Some refactoring

--- src/main/java/com/example/blablacar/service/UserService.java
+++ src/main/java/com/example/blablacar/service/UserService.java
@@ -1,14 +1,13 @@
 package com.example.blablacar.service;
 
 import com.example.blablacar.dto.LoginRequest;
-import com.example.blablacar.dto.RegisterRequest;
+import com.example.blablacar.dto.UserRegistrationRequest;
 import com.example.blablacar.model.User;
-import org.springframework.stereotype.Service;
 
 
 public interface UserService {
 
-    User registerUser(User user);
+    User registerUser(UserRegistrationRequest registerRequest);
 
     String verify(LoginRequest user);
 }
