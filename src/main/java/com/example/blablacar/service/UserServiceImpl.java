1eeba2d9cf1832b4c04a1a22ad0a01aadcec58ac
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-06-13 20:33:36 +0300

    Use Builder pattern and add custom exception

--- src/main/java/com/example/blablacar/service/UserServiceImpl.java
+++ src/main/java/com/example/blablacar/service/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.blablacar.service;
 
 import com.example.blablacar.dto.LoginRequest;
+import com.example.blablacar.dto.UserRegistrationRequest;
+import com.example.blablacar.enums.AuthProvider;
+import com.example.blablacar.enums.Role;
+import com.example.blablacar.exception.EmailAlreadyExistsException;
 import com.example.blablacar.model.User;
 import com.example.blablacar.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,8 +29,22 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User registerUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public User registerUser(UserRegistrationRequest userRegistrationRequest) {
+        if (userRepository.existsByEmail(userRegistrationRequest.getEmail())) {
+            throw new EmailAlreadyExistsException("Email is already registered");
+        }
+        userRegistrationRequest.setPassword(passwordEncoder.encode(userRegistrationRequest.getPassword()));
+        User user = User.builder()
+                .name(userRegistrationRequest.getName())
+                .email(userRegistrationRequest.getEmail())
+                .password(userRegistrationRequest.getPassword())
+                .provider(AuthProvider.LOCAL)
+                .providerId(null)
+                .role(Role.ROLE_USER)
+                .birthday(userRegistrationRequest.getBirthday())
+                .phoneNumber(userRegistrationRequest.getPhoneNumber())
+                .gender(userRegistrationRequest.getGender())
+                .build();
         return userRepository.save(user);
     }
 
