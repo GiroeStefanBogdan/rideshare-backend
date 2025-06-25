827c5a25d7b0ee930ac53ebd8f80ecd24d8549dc
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-06-25 23:35:07 +0300

    Change from class UserRegistrationRequest to record  UserRegistrationRequest

--- src/main/java/com/example/blablacar/dto/UserRegistrationRequest.java
+++ src/main/java/com/example/blablacar/dto/UserRegistrationRequest.java
@@ -2,36 +2,18 @@ package com.example.blablacar.dto;
 
 import com.example.blablacar.enums.Gender;
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Setter
-@Getter
-public class UserRegistrationRequest {
-    @NotBlank(message = "Name is required")
-    private String name;
+public record UserRegistrationRequest(@NotBlank(message = "Name is required") String name,
 
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email is required")
-    private String email;
+                                      @Email(message = "Email should be valid") @NotBlank String email,
 
-    @Size(min = 6, message = "Password must be at least 6 characters")
-    @NotBlank(message = "Password is required")
-    private String password;
+                                      @Size(min = 6, message = "Password must be at least 6 characters") @NotBlank(message = "Password is required") String password,
 
-    @NotNull(message = "Birthday is required and must be in the past")
-    @Past
-    private LocalDate birthday;
+                                      @NotNull(message = "Birthday is required and must be in the past") @Past LocalDate birthday,
 
-    @NotBlank(message = "Phone number is required")
-    @Pattern(
-            regexp = "^(\\+4|)?(07[0-8][0-9]|02[0-9]{2}|03[0-9]{2})(\\s|\\.|\\-)?[0-9]{3}(\\s|\\.|\\-)?[0-9]{3}$",
-            message = "Invalid Romanian phone number"
-    )
-    private String phoneNumber;
+                                      @NotBlank(message = "Phone number is required") @Pattern(regexp = "^(\\+4|)?(07[0-8][0-9]|02[0-9]{2}|03[0-9]{2})(\\s|\\.|\\-)?[0-9]{3}(\\s|\\.|\\-)?[0-9]{3}$", message = "Invalid Romanian phone number") String phoneNumber,
 
-    @NotNull(message = "gender is required")
-    private Gender gender;
+                                      @NotNull(message = "gender is required") Gender gender) {
 }
