c047f1bdacec889aaebd16c8bbe204348a332ab3
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-06-25 23:33:14 +0300

    Refactor the names

--- src/main/java/com/example/blablacar/enums/Gender.java
+++ src/main/java/com/example/blablacar/enums/Gender.java
@@ -4,6 +4,6 @@ package com.example.blablacar.enums;
  * Enum representing the sex of a user (M or F)
  */
 public enum Gender {
-    M, // Male
-    F  // Female
+    MALE, // Male
+    FEMALE  // Female
 }
