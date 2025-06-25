a590be94d50c94f4e486b4b97221f78827a98f9f
Giroe Stefan Bogdan <Giroe_1999@proton.me>
2025-06-25 23:34:04 +0300

    Add @Validated as class level

--- src/main/java/com/example/blablacar/controller/UserController.java
+++ src/main/java/com/example/blablacar/controller/UserController.java
@@ -10,12 +10,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 
+@Validated
 @RestController
 public class UserController {
 
